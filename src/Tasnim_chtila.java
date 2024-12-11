370bfe203636b7ac8d6b1ff16c7af90f236e889a
yasmine-cloud <chtilatasnim@gmail.com>
2024-12-11 19:28:49 +0100

    tasnim commit

---
+++ src/Tasnim_chtila.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tasnim_chtila {
+    public static int calculateSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+}
