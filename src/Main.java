35b6c19cfdadd44ca27eb5f657ace40cf52587a0
yasmine-cloud <chtilatasnim@gmail.com>
2024-12-11 19:34:51 +0100

    tasnim commit3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Combien de nombres voulez-vous additionner ?");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Entrez le nombre " + (i + 1) + " :");
+            numbers[i] = scanner.nextInt();
         }
+
+        // Appel à la méthode de la classe SumCalculator
+        int sum = Tasnim_chtila.calculateSum(numbers);
+
+        System.out.println("La somme des nombres est : " + sum);
+        scanner.close();
     }
 }
