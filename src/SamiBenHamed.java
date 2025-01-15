4aa9a4a4a8067b33124b4887224f7c45adc0cbee
sami ben hamed <benhamedsami72@gmail.com>
2025-01-15 11:21:51 +0100

    ajout f calcul factoriel

--- src/SamiBenHamed.java
+++ src/SamiBenHamed.java
@@ -1,3 +1,12 @@
 public class SamiBenHamed {
-    
+    public static int claculFactorierl(int nb ){
+        int fact = 1 ;
+        if(nb==1 || nb=0 ){
+            return 1 ; 
+        }
+        for (int i=2 ; i<=nb ; i++ ){
+            fact = fact * i ; 
+        }
+        return fact ; 
+    }
 }
