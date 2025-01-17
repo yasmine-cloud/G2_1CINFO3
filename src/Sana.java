0810068848e27a9168a0b55f8cf3c6b9256da0aa
Mathlouthi Sana <Sana.Mathlouthi@gat.com.tn>
2025-01-17 22:27:32 +0100

    Sana2avec S apres conflit

--- src/Sana.java
+++ src/Sana.java
@@ -24,14 +24,31 @@ public class Sana {
 
     // Méthode pour calculer la moyenne
     public static double calculerMoyenne(int[] tableau) {
-        if (tableau.length == 0) {
-            return 0; // Eviter la division par zéro si le tableau est vide
-        }
-        int somme = calculerSomme(tableau);
-        return (double) somme / tableau.length;
 
-        //say hello
+        public static double calculerMoyenne(int[] tableau) {
+
+            if (tableau.length == 0) {
+                return 0; // Eviter la division par zéro si le tableau est vide
+            }
+            int somme = calculerSomme(tableau);
+            return (double) somme / tableau.length;
+
+            //say hello
+
+        }
+
     }
+    public static int calculerSomme(int[] tableau) {
+        int somme = 0;
+=======
+        public static int calculerSomme(int[] tableau) {
+            int somme = 1;
+>>>>>>> main
+            for (int i = 0; i < tableau.length; i++) {
+                somme *= tableau[i];
+            }
+            return somme;
+        }
     }
 
 
