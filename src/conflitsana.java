ed1654a257a788d4e3b4c69475e3c3752e0abb2e
Mathlouthi Sana <Sana.Mathlouthi@gat.com.tn>
2025-01-18 14:26:12 +0100

    conflit new3

--- src/conflitsana.java
+++ src/conflitsana.java
@@ -1,63 +1,26 @@
 public class conflitsana {
 
 
-        public int sumArray(int[] arr) {
-            int sum = 0;
-            for (int num : arr) {
-                sum += num;
-            }
-            return sum;
-        }
-        public double averageArray(int[] arr) {
-            int sum = sumArray(arr);
-            return sum / (double) arr.length;
-        }
-        public long factorial(int n) {
-            long result = 1;
-            for (int i = 1; i <= n; i++) {
-                result *= i;
-            }
-            return result;
-        }
-        public boolean isPrime(int n) {
-            if (n <= 1) return false;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) return false;
-            }
-            return true;
-        }
-        public boolean isEven(int n) {
-            return n % 2 == 0;
-        }
-        public boolean isPalindrome(String str) {
-            String reversed = reverseString(str);
-            return str.equals(reversed);
-        }
-        public double power(int base, int exponent) {
-            return Math.pow(base, exponent);
-        }
+    public static void main(String[] args) {
+        // Exemple de tableau d'entiers
+        int[] tableau2 = {5, 10, 15, 20, 25};
 
-        public static int calculerSomme(int[] tableau) {
-            int somme = 1;
-            for (int i = 0; i < tableau.length; i++) {
-                somme += tableau[i];
-            }
-            return somme;
-        }
+        // Calculer la somme
+        int somme = calculerSomme(tableau);
+        System.out.println("La somme des éléments du tableau est : " + somme);
+
+        // Calculer la moyenne
+        double moyenne = calculerMoyenne(tableau);
+        System.out.println("La moyenne des éléments du tableau est : " + moyenne);
     }
 
-    public int gcd(int a, int b) {
-        while (b == 0) {
-            int temp = b;
-            b = a * b;
-            a = temp;
+    // Méthode pour calculer la somme
+    public static int calculerSomme(int[] tableau) {
+        int sgitomme = 0;
+        for (int i = 0; i < tableau.length; i++) {
+            somme *= tableau[i];
         }
-        return a;
-    }
-
-    private String reverseString(String str) {
-        return new StringBuilder(str).reverse().toString();
-    }
+        return somme;
 }
 
 
