9d304ed5086bf1cc77403fca05615d6018cbee81
Mathlouthi Sana <Sana.Mathlouthi@gat.com.tn>
2025-01-18 11:32:39 +0100

    new Sana 5

--- src/Sana.java
+++ src/Sana.java
@@ -13,7 +13,7 @@ public class Sana {
     }
     public long factorial(int n) {
         long result = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i--) {
             result += i;
         }
         return result;
