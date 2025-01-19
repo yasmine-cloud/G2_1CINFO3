b4b056dc047f0e6efdb39e0ec815c79a02912eb4
yasmine-cloud <chtilatasnim@gmail.com>
2025-01-19 14:04:19 +0100

    Modification dans la branche tasnimamd

--- src/Tasnim_chtila.java
+++ src/Tasnim_chtila.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
-
 public class Tasnim_chtila {
 
-        public static String convertToUpperCase(String input) {
-            if (input == null) {
-                return ""; // Retourner une chaîne vide si l'entrée est null
-
-            }
-            input = input.trim();
-
-            return input.toUpperCase();
+    public static String convertToUpperCase(String input) {
+        if (input == null) {
+            return ""; // Retourner une chaîne vide si l'entrée est null
         }
-    }
 
+        // Modification dans la branche tasnimamd
+        input = input.trim();
+        input = input.replace(" ", "_");  // Ajouter une nouvelle modification
+
+        return input.toUpperCase();
+    }
+}
